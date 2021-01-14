fef61f04fad85894f56b15b32296a820a9b2b899
Kimhooo <327734079@qq.com>
2021-01-14 09:27:08 +0800

    Create 31days.kt
    add day1

---
+++ 31days.kt
@@ -0,0 +1,7 @@
+day1：
+everything in kotlin is public by default.you can also use private , internal , protected.
+like:
+val isVisible = true
+private val isHidden = true
+internal val almostVisible = true
+protected val isInHeritable = true 
